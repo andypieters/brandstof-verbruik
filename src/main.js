add6b80765d129b76fb9c0ab2b55d4c7874f9efe
andypieters <andy@andypieters.nl>
2020-01-25 12:00:38 +0100

    Added vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import store from './store'
 
 
 Vue.config.productionTip = false
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
